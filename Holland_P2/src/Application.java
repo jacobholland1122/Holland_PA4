e35af4f78de3a566a5791af5bc011a0d814ed830
Jacob Holland <jacobholland1122@knights.ucf.edu>
2019-11-02 03:29:52 -0400

    final commit

--- Holland_P2/src/Application.java
+++ Holland_P2/src/Application.java
@@ -5,7 +5,7 @@ public class Application {
     public static void main(String[] args) throws IOException {
 
         String dataFile = "problem2.txt";
-        String outputFile = "unique_words_counts.txt";
+        String outputFile = "unique_word_counts.txt";
 
         DuplicateCounter duplicateCounter = new DuplicateCounter();
 
